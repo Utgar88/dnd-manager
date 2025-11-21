a95b766234b43089171aa7e98a233703f39e35c4
Dirk Pillich <dirk.pillich@outlook.de>
2025-11-21 13:25:22 +0100

    Projektdateien hinzufügen.

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DndManager")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DndManager.Pages")]
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DndManager
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
